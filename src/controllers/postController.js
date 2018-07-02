7942ce93b445763cc38ed2138c3a11129bac9f03
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2018-07-02 20:58:50 -0300

    colocando paginação na rota de posts por categoria

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,49 +1,53 @@
 import Post from '../models/Post';
 import Category from '../models/Category';
 
-const PER_PAGE = 10;
+const PER_PAGE = 2;
 
 export default {
     listAll: async (req, res) => {
         try {
-
+            // Pegando elementos da uri
             let { q, page } = req.query;
 
+            // Convertendo page para int se tiver 
             page = parseInt(page ? page : 1);
+            // Importante para fazer a busca
             q = q ? q : "";
 
-            const total = await Post.find({
+            // Categorias que tem o texto (q). Importante para ser mais um parâmetro na busca
+            const categoriesId = await Category.find({ name: { "$regex": q, "$options": "i" } }).distinct('_id');
+
+            // Construindo a query principal da rota
+            const query = {
                 $or: [
-                    { title: { "$regex": q, "$options": "i" } }, { body: { "$regex": q, "$options": "i" } }
+                    { title: { "$regex": q, "$options": "i" } },
+                    { body: { "$regex": q, "$options": "i" } },
+                    { slug: { "$regex": q, "$options": "i" } },
+                    { categories: { $in: categoriesId } },
                 ]
-            }).count();
+            };
+
+            // Quantidade de todos os retornos possíveis da requisição. Importante para a paginação
+            const total = await Post.find(query).count();
 
             // Calcular a última página
-            if (total % PER_PAGE == 0) {
-                lastPage = parseInt(total / PER_PAGE);
-            } else {
-                lastPage = parseInt((total / PER_PAGE) + 1);
-            }
+            const firstPage = 1;
+            const lastPage = parseInt(total / PER_PAGE) + (total % PER_PAGE == 0 ? 0 : 1);
 
-            // Evitar esforços
+            // Evitar esforços, se a página da requisição não tem resultado, já retorna vazio
             if (page > lastPage) {
                 return res.send({
                     posts: [],
-                    result: 0
+                    result: 0,
+                    total: total
                 });
             }
 
-            const posts = await Post.find({
-                $or: [
-                    { title: { "$regex": q, "$options": "i" } }, { body: { "$regex": q, "$options": "i" } }
-                ]
-            }).populate({
+            // Busca efetiva dos posts com a paginação e limite por página. 
+            // Usando a query principal.
+            const posts = await Post.find(query).populate({
                 path: "categories"
             }).skip(PER_PAGE * page - PER_PAGE).limit(PER_PAGE);
-
-            const firstPage = 1;
-            let lastPage;
-
 
             return res.send({
                 posts,
@@ -52,7 +56,7 @@ export default {
                 total,
                 q,
                 nextPage: page == lastPage ? null : page + 1,
-                prevPage: page == firstPage ? null : page - 1
+                prevPage: page == firstPage ? null : page - 1,
             });
         } catch (err) {
             return res.status(400).send({
@@ -83,21 +87,53 @@ export default {
         try {
             const category = await Category.findOne({ slug: req.params.slug });
 
+            // Se não tiver categoria com a slug da requisição
             if (!category) {
                 return res.send({
                     posts: [],
-                    result: 0
+                    result: 0,
+                    total: 0
+                });
+            };
+
+            // Pegando elementos da uri
+            let { page } = req.query;
+
+            // Convertendo page para int se tiver 
+            page = parseInt(page ? page : 1);
+
+            // Quantidade de todos os retornos possíveis da requisição. Importante para a paginação
+            const total = await Post.find({ categories: category._id }).count();
+
+            // Calcular a última página
+            const firstPage = 1;
+            const lastPage = parseInt(total / PER_PAGE) + (total % PER_PAGE == 0 ? 0 : 1);
+
+            // Evitar esforços, se a página da requisição não tem resultado, já retorna vazio
+            if (page > lastPage) {
+                return res.send({
+                    posts: [],
+                    result: 0,
+                    total: total
                 });
             }
 
+            // Busca efetiva dos posts com a paginação e limite por página.
             const posts = await Post.find({ categories: category._id }).populate({
                 path: 'categories',
                 select: 'name slug'
-            });
+            }).skip(PER_PAGE * page - PER_PAGE).limit(PER_PAGE);
+
+            console.log(req);
 
             return res.send({
                 posts,
-                result: posts.length
+                result: posts.length,
+                page,
+                total,
+                category: req.params.slug,
+                nextPage: page == lastPage ? null : page + 1,
+                prevPage: page == firstPage ? null : page - 1,
             });
 
         } catch (err) {
